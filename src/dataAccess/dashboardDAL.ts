0f2fc593688d9d124f7eb200086a34bae0bbf9a3
yash2376 <yash.variya@tatvasoft.com>
2025-12-16 16:30:52 +0530

    Fix the bug

--- src/dataAccess/dashboardDAL.ts
+++ src/dataAccess/dashboardDAL.ts
@@ -3,10 +3,10 @@ import { AxiosGet, AxiosPost } from "../utils/axios";
 
 const ENDPOINT = "api/Dashboard";
 
-export const GetDashboardData = async (userId : number | string, month : number, type : string): Promise<any> => {
-let result = await AxiosGet(ENDPOINT + `/GetDashboardData/${userId}/${month}/${type}`);
-    return result.data;
-  };
+export const GetDashboardData = async (userId: number | string, month: number, type: string): Promise<any> => {
+  let result = await AxiosGet(ENDPOINT + `/GetDashboardData/${userId}/${month}/${type}`);
+  return result?.data;
+};
 
 export const addUserLimit = async (payload: LimitPayload) => {
   return await AxiosPost("api/Auth/AddLimit", payload);
